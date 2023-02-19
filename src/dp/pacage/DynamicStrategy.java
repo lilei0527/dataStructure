b1aa602cba4ef84d8666fbeffeffc829ed3fbd9e
李磊 <15623219507@163.com>
2023-02-19 19:12:44 +0800

    feat:青蛙跳台阶

--- src/dp/pacage/DynamicStrategy.java
+++ src/dp/pacage/DynamicStrategy.java
@@ -2,6 +2,7 @@ package dp.pacage;
 
 /**
  * @author lilei
+ * 动态规划核心就在于拆分子问题，记住过往，减少重复计算
  * 动态规划解决01背包
  * 时间复杂度：cap*n (cap:背包容量,n:物品数量)
  **/
@@ -31,5 +32,24 @@ public class DynamicStrategy implements Strategy {
             System.arraycopy(results, 0, preResults, 0, cap + 1);
         }
         return preResults[cap];
+    }
+
+    //一只青蛙一次可以跳上1级台阶，也可以跳上2级台阶。求该青蛙跳上一个 10 级的台阶总共有多少种跳法
+    public int numWays(int n) {
+        if (n<= 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        int a = 1;
+        int b = 2;
+        int temp = 0;
+        for (int i = 3; i <= n; i++) {
+            temp = a + b;
+            a = b;
+            b = temp;
+        }
+        return temp;
     }
 }
