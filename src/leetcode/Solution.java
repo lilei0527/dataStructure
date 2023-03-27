c7e6ab4cfb2d9fa762ccfc462b1aa7cdfc63d136
李磊 <15623219507@163.com>
2023-03-27 19:26:43 +0800

    feat: 跳跃游戏

--- src/leetcode/Solution.java
+++ src/leetcode/Solution.java
@@ -9,6 +9,38 @@ import java.util.*;
 @SuppressWarnings("unused")
 public class Solution {
     /**
+     * 跳跃游戏
+     * 给定一个非负整数数组 nums ，你最初位于数组的 第一个下标 。
+     *
+     * 数组中的每个元素代表你在该位置可以跳跃的最大长度。
+     *
+     * 判断你是否能够到达最后一个下标。
+     *
+     *
+     *
+     * 示例1：
+     *
+     * 输入：nums = [2,3,1,1,4]
+     * 输出：true
+     * 解释：可以先跳 1 步，从下标 0 到达下标 1, 然后再从下标 1 跳 3 步到达最后一个下标。
+     * 示例2：
+     *
+     * 输入：nums = [3,2,1,0,4]
+     * 输出：false
+     * 解释：无论怎样，总会到达下标为 3 的位置。但该下标的最大跳跃长度是 0 ， 所以永远不可能到达最后一个下标。
+     *
+     */
+    public boolean canJump(int[] nums) {
+        int canJumpMinIndex = nums.length-1;
+        for (int i = nums.length-2; i >=0; i--) {
+            if(nums[i]>=canJumpMinIndex-i){
+                canJumpMinIndex = i;
+            }
+        }
+        return canJumpMinIndex == 0;
+    }
+
+    /**
      * 最大子数组和
      *
      * 给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
