9a1331250f01f38b9a56dde589dfca11fe81009d
李磊 <15623219507@163.com>
2023-03-28 12:47:01 +0800

    feat: 合并区间

--- src/leetcode/Solution.java
+++ src/leetcode/Solution.java
@@ -9,6 +9,44 @@ import java.util.*;
 @SuppressWarnings("unused")
 public class Solution {
     /**
+     * 以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi] 。请你合并所有重叠的区间，并返回一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间。
+     *
+     *
+     *
+     * 示例 1：
+     *
+     * 输入：intervals = [[1,3],[2,6],[8,10],[15,18]]
+     * 输出：[[1,6],[8,10],[15,18]]
+     * 解释：区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+     * 示例2：
+     *
+     * 输入：intervals = [[1,4],[4,5]]
+     * 输出：[[1,5]]
+     * 解释：区间 [1,4] 和 [4,5] 可被视为重叠区间。
+     *
+     */
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, Comparator.comparingInt(interval -> interval[0]));
+
+        List<int[]>list = new ArrayList<>();
+        list.add(intervals[0]);
+
+        for (int i = 1; i < intervals.length; i++) {
+            int[] last = list.get(list.size() - 1);
+            if(intervals[i][0]<=last[1]){
+                //重叠
+                last[1]=Math.max(intervals[i][1],last[1]);
+            }else {
+                list.add(intervals[i]);
+            }
+        }
+
+        return list.toArray(new int[list.size()][]);
+
+    }
+
+
+    /**
      * 跳跃游戏
      * 给定一个非负整数数组 nums ，你最初位于数组的 第一个下标 。
      *
