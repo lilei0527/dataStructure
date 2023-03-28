0a97994b525a012ff3335b1fb5e9ea5e3c99b485
李磊 <15623219507@163.com>
2023-03-28 14:40:13 +0800

    feat: 颜色分类

--- src/leetcode/Solution.java
+++ src/leetcode/Solution.java
@@ -9,6 +9,70 @@ import java.util.*;
 @SuppressWarnings("unused")
 public class Solution {
     /**
+     * 给定一个包含红色、白色和蓝色、共n 个元素的数组nums，原地对它们进行排序，使得相同颜色的元素相邻，并按照红色、白色、蓝色顺序排列。
+     *
+     * 我们使用整数 0、1 和 2 分别表示红色、白色和蓝色。
+     *
+     * 必须在不使用库内置的 sort 函数的情况下解决这个问题。
+     */
+    public void sortColors(int[] nums) {
+        Map<Integer,Integer>map=new HashMap<>();
+        for (int num : nums) {
+            Integer integer = map.get(num);
+            if(integer==null){
+                map.put(num,1);
+            }else {
+                map.put(num,integer+1);
+            }
+        }
+
+        Integer integer1 = map.get(0);
+        Integer integer2 = map.get(1);
+        Integer integer3 = map.get(2);
+        if(integer1==null){
+            integer1=0;
+        }
+        if(integer2==null){
+            integer2=0;
+        }
+        if(integer3==null){
+            integer3=0;
+        }
+        for (int i = 0; i < integer1; i++) {
+            nums[i]=0;
+        }
+        for (int i = integer1; i < integer1+integer2; i++) {
+            nums[i]=1;
+        }
+        for (int i = integer1+integer2; i < integer1+integer2+integer3; i++) {
+            nums[i]=2;
+        }
+    }
+
+    //双指针
+    public void sortColors1(int[] nums) {
+        if(nums.length<2){
+            return;
+        }
+        int i=0;
+        int p0=0;
+        int p2 = nums.length-1;
+        while (i<=p2){
+            if(nums[i]==0){
+                swap(nums,i,p0);
+                i++;
+                p0++;
+            }else if(nums[i]==1){
+                i++;
+            }else {
+                swap(nums,i,p2);
+                p2--;
+            }
+        }
+    }
+
+
+    /**
      * 不同路径
      * 一个机器人位于一个 m x n网格的左上角 （起始点在下图中标记为 “Start” ）。
      *
