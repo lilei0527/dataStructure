0419ef755055786931949235396242fd2ffb80cc
李磊 <15623219507@163.com>
2023-04-01 19:21:11 +0800

    feat: 零钱兑换

--- src/leetcode/Solution.java
+++ src/leetcode/Solution.java
@@ -9,6 +9,35 @@ import java.util.*;
 @SuppressWarnings("unused")
 public class Solution {
 
+
+    /**
+     *  零钱兑换
+     * 给你一个整数数组 coins ，表示不同面额的硬币；以及一个整数 amount ，表示总金额。
+     *
+     * 计算并返回可以凑成总金额所需的 最少的硬币个数 。如果没有任何一种硬币组合能组成总金额，返回-1 。
+     *
+     * 你可以认为每种硬币的数量是无限的。
+     */
+    public int coinChange(int[] coins, int amount) {
+        int []dp = new int[amount+1];
+        Arrays.fill(dp,amount+1);
+        dp[0]=0;
+
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if(coins[j]<=i){
+                    //总金额为i的最小硬币数量
+                    dp[i] = Math.min(dp[i],dp[i-coins[j]]+1);
+                }
+            }
+        }
+
+        if(dp[amount]>amount){
+            return -1;
+        }
+        return dp[amount];
+    }
+
     /**
      * 最大正方形
      * 在一个由 '0' 和 '1' 组成的二维矩阵内，找到只包含 '1' 的最大正方形，并返回其面积。
