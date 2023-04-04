44f31f214c73d3e6542b8d96f18ac2b5d69c032d
李磊 <15623219507@163.com>
2023-04-04 16:07:51 +0800

    feat: 寻找重复数

--- src/leetcode/Solution.java
+++ src/leetcode/Solution.java
@@ -10,6 +10,45 @@ import java.util.*;
  **/
 @SuppressWarnings("unused")
 public class Solution {
+
+
+    /**
+     * 寻找重复数
+     * 给定一个包含n + 1 个整数的数组nums ，其数字都在[1, n]范围内（包括 1 和 n），可知至少存在一个重复的整数。
+     *
+     * 假设 nums 只有 一个重复的整数 ，返回这个重复的数 。
+     *
+     * 你设计的解决方案必须 不修改 数组 nums 且只用常量级 O(1) 的额外空间。
+     *
+     *
+     *
+     * 示例 1：
+     *
+     * 输入：nums = [1,3,4,2,2]
+     * 输出：2
+     * 示例 2：
+     *
+     * 输入：nums = [3,1,3,4,2]
+     * 输出：3
+     */
+    public int findDuplicate(int[] nums) {
+        int slow =0;
+        int fast =0;
+
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        fast = 0;
+        while (fast!=slow){
+            fast = nums[fast];
+            slow = nums[slow];
+        }
+
+        return fast;
+    }
+
     /**
      * 搜索二维矩阵 II
      * 编写一个高效的算法来搜索 m x n 矩阵 matrix 中的一个目标值 target 。该矩阵具有以下特性：
