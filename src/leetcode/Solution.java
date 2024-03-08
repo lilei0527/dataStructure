00cd76727171fa051b52f9d37a1d92684216b60a
lilei <15623219507@163.com>
2024-03-08 14:32:13 +0800

    (feat) 两两交换链表中的节点

--- src/leetcode/Solution.java
+++ src/leetcode/Solution.java
@@ -294,7 +294,26 @@ public class Solution {
             }
         }
 
-        //
+        //两两交换链表中的节点
+        public ListNode swapPairs(ListNode head) {
+            ListNode dummy = new ListNode(-1);
+            dummy.next = head;
+            ListNode cur = dummy;
+            while (cur.next!=null&&cur.next.next!=null){
+
+                ListNode node1 = cur.next;
+                ListNode node2 = cur.next.next;
+
+                cur.next = node2;
+                node1.next = node2.next;
+                node2.next = node1;
+
+                cur = node1;
+            }
+            return dummy.next;
+        }
+
+
         public ListNode removeNthFromEnd(ListNode head, int n) {
             ListNode fast = head;
             for (int i = 0; i < n; i++) {
