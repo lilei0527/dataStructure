9e2d5721832f1e8f26eb8e2c47aea910ca368550
李磊 <15623219507@163.com>
2024-03-09 21:14:26 +0800

    property

--- src/leetcode/Solution.java
+++ src/leetcode/Solution.java
@@ -12,6 +12,27 @@ import java.util.stream.Collectors;
 @SuppressWarnings("unused")
 public class Solution {
     /**
+     * 轮转数组
+     * 给定一个整数数组 nums，将数组中的元素向右轮转 k 个位置，其中 k 是非负数。
+     */
+    public void rotate(int[] nums, int k) {
+        int offset = k % nums.length;
+        reverse(nums,0,nums.length-1);
+        reverse(nums,0,offset-1);
+        reverse(nums,offset,nums.length-1);
+    }
+
+    public void reverse(int[] nums,int start,int end){
+        while (start<end){
+            int temp=nums[start];
+            nums[start]=nums[end];
+            nums[end]=temp;
+            start++;
+            end--;
+        }
+    }
+
+    /**
      * 给定一个经过编码的字符串，返回它解码后的字符串。
      *
      * 编码规则为: k[encoded_string]，表示其中方括号内部的 encoded_string 正好重复 k 次。注意 k 保证为正整数。
@@ -2811,7 +2832,7 @@ public class Solution {
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Solution solution = new Solution();
 //        Ip ip = new Ip();
 //        List<String> ips = ip.getIps("1231232123");
@@ -2950,6 +2971,12 @@ public class Solution {
 //        System.out.println(Arrays.toString(ints));
 
         System.out.println(solution.decodeString("2[abc]3[cd]ef"));
+
+        for (int i = 0; i < 10; i++) {
+            Thread.sleep(100000);
+            System.out.println("xiuxi");
+        }
+
 
     }
 }
