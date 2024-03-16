2f1dd1ad05c2ebe3ee19b5703e3110576cca8b98
李磊 <15623219507@163.com>
2024-03-16 18:20:58 +0800

    feat: 翻转二叉树

--- src/leetcode/Solution.java
+++ src/leetcode/Solution.java
@@ -349,6 +349,17 @@ public class Solution {
         return Math.max(maxDepth(root.left),maxDepth(root.right))+1;
     }
 
+    //翻转二叉树
+    public TreeNode invertTree(TreeNode root) {
+        if(root==null){
+            return null;
+        }
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        root.right = left;
+        root.left = right;
+        return root;
+    }
 
     /**
      *  零钱兑换
