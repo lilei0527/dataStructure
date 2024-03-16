d3f856d31984e144d4c28773c0b92a733abb47cf
李磊 <15623219507@163.com>
2024-03-16 17:58:51 +0800

    feat: 二叉树的最大深度

--- src/leetcode/Solution.java
+++ src/leetcode/Solution.java
@@ -341,7 +341,13 @@ public class Solution {
         midDfs(node.right,list);
     }
 
-
+    // 二叉树的最大深度
+    public int maxDepth(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+        return Math.max(maxDepth(root.left),maxDepth(root.right))+1;
+    }
 
 
     /**
