da1ef9bf7e9eab7ab7d0bfc2a097b9de212818ad
李磊 <15623219507@163.com>
2024-03-16 20:58:54 +0800

    feat:验证二叉搜索树

--- src/leetcode/Solution.java
+++ src/leetcode/Solution.java
@@ -756,22 +756,19 @@ public class Solution {
             this.right = right;
         }
     }
-    public boolean isValidBST(TreeNode root) {
-        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+
+    //验证二叉搜索树
+    public boolean isValidBST1(TreeNode root){
+        return idValidBST1(root,Long.MIN_VALUE,Long.MAX_VALUE);
     }
 
-    public boolean isValidBST(TreeNode node, long lower, long upper) {
-        if (node == null) {
+    public boolean idValidBST1(TreeNode node,long min,long max){
+        if(node==null){
             return true;
         }
-        if (node.val <= lower || node.val >= upper) {
-            return false;
-        }
-
-        return isValidBST(node.left, lower, node.val) &&
-                isValidBST(node.right, node.val, upper);
+        return node.val>min&&node.val<max
+                &&idValidBST1(node.left,min,node.val)&&idValidBST1(node.right,node.val,max);
     }
-
 
     /**
      * 层序遍历
