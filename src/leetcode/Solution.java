85f3217337babb617fbeaa77938c23af41fcf605
李磊 <15623219507@163.com>
2024-03-23 18:08:25 +0800

    feat：爬楼梯

--- src/leetcode/Solution.java
+++ src/leetcode/Solution.java
@@ -3218,6 +3218,23 @@ public class Solution {
         }
 
 
+        //爬楼梯
+        //假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+        //每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+        public int climbStairs(int n) {
+            if(n==1){
+                return 1;
+            }
+            int []ints = new int[n+1];
+            ints[2]=2;
+            ints[1]=1;
+            for (int i = 3; i <= n; i++) {
+                ints[i] = ints[i-1]+ints[i-2];
+            }
+            return ints[n];
+        }
+
+
 
 
     public static void main(String[] args) throws InterruptedException {
