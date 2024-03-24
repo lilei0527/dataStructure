ea2978575e9324c7dd9653eb03e6872cf99ee1e3
李磊 <15623219507@163.com>
2024-03-24 21:05:52 +0800

    feat：零钱兑换

--- src/leetcode/Solution.java
+++ src/leetcode/Solution.java
@@ -467,20 +467,41 @@ public class Solution {
         Arrays.fill(dp,amount+1);
         dp[0]=0;
 
-        for (int i = 1; i <= amount; i++) {
-            for (int j = 0; j < coins.length; j++) {
-                if(coins[j]<=i){
+        for (int j = 0; j < coins.length; j++) {
+            for (int i = coins[j]; i <= amount; i++) {
                     //总金额为i的最小硬币数量
                     dp[i] = Math.min(dp[i],dp[i-coins[j]]+1);
-                }
             }
         }
-
         if(dp[amount]>amount){
             return -1;
         }
         return dp[amount];
     }
+
+
+    //零钱兑换
+    //给你一个整数数组 coins 表示不同面额的硬币，另给一个整数 amount 表示总金额。
+    //
+    //请你计算并返回可以凑成总金额的硬币组合数。如果任何硬币组合都无法凑出总金额，返回 0 。
+    //
+    //假设每一种面额的硬币有无限个。
+    //
+    //题目数据保证结果符合 32 位带符号整数。
+    public int change(int amount, int[] coins) {
+        int []dp=new int[amount+1];
+        dp[0] = 1;
+        for (int i = 0; i < coins.length; i++) {
+            for (int j = coins[i]; j <= amount ; j++) {
+                dp[j] = dp[j]+dp[j-coins[i]];
+            }
+        }
+        return dp[amount];
+    }
+
+
+
+
 
     /**
      * 最大正方形
