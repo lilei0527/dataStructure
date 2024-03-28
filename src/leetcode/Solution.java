64c72c6396855ccfe21a7b83bb1cc123d6446753
李磊 <15623219507@163.com>
2024-03-28 22:43:51 +0800

    feat：课程表

--- src/leetcode/Solution.java
+++ src/leetcode/Solution.java
@@ -3345,6 +3345,57 @@ public class Solution {
         return step;
     }
 
+    //课程表
+    //你这个学期必须选修 numCourses 门课程，记为 0 到 numCourses - 1 。
+    //
+    //在选修某些课程之前需要一些先修课程。 先修课程按数组 prerequisites 给出，其中 prerequisites[i] = [ai, bi] ，表示如果要学习课程 ai 则 必须 先学习课程  bi 。
+    //
+    //例如，先修课程对 [0, 1] 表示：想要学习课程 0 ，你需要先完成课程 1 。
+    //请你判断是否可能完成所有课程的学习？如果可以，返回 true ；否则，返回 false 。
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+            int []in = new int[numCourses];
+            List<List<Integer>>out = new ArrayList<>();
+
+        for (int i = 0; i < numCourses; i++) {
+            out.add(new ArrayList<>());
+
+        }
+
+
+        //计算每个节点的出度集合
+        for (int[] prerequisite : prerequisites) {
+            List<Integer> integers = out.get(prerequisite[0]);
+            integers.add(prerequisite[1]);
+            in[prerequisite[1]]++;
+        }
+
+        Queue<Integer>queue = new LinkedList<>();
+        //将入度为0的入队
+        for (int i = 0; i < numCourses; i++) {
+            if(in[i]==0){
+                queue.add(i);
+            }
+        }
+
+        int step = 0;
+        while (!queue.isEmpty()){
+            step++;
+            Integer poll = queue.poll();
+            List<Integer> integers = out.get(poll);
+
+            if(integers!=null){
+                for (Integer integer : integers) {
+                    in[integer]--;
+                    if(in[integer]==0){
+                        queue.add(integer);
+                    }
+                }
+            }
+        }
+
+        return step==numCourses;
+    }
 
 
     public static void main(String[] args) throws InterruptedException {
