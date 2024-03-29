55f84c20468ef4b0895a847679e376ce9d45ebaf
李磊 <15623219507@163.com>
2024-03-29 21:58:05 +0800

    feat：搜索二维矩阵

--- src/leetcode/Solution.java
+++ src/leetcode/Solution.java
@@ -3417,6 +3417,31 @@ public class Solution {
 
     }
 
+    //搜索二维矩阵
+    //给你一个满足下述两条属性的 m x n 整数矩阵：
+    //
+    //每行中的整数从左到右按非严格递增顺序排列。
+    //每行的第一个整数大于前一行的最后一个整数。
+    //给你一个整数 target ，如果 target 在矩阵中，返回 true ；否则，返回 false 。
+    public boolean searchMatrix1(int[][] matrix, int target) {
+        for (int i = 0; i < matrix.length; i++) {
+            if(matrix[i][matrix[0].length-1]>=target){
+                int left = 0;int right =matrix[0].length-1;
+                while (left<=right){
+                    int mid = (left+right)/2;
+                    if(matrix[i][mid]<target){
+                        left=mid+1;
+                    }else if(matrix[i][mid]>target){
+                        right=mid-1;
+                    }else {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
 
     public static void main(String[] args) throws InterruptedException {
         Solution solution = new Solution();
