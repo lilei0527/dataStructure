967b3c60305b206ab1668a6abf6b0b7294bb1597
lilei <15623219507@163.com>
2024-04-01 15:30:10 +0800

    feat：编辑距离

--- src/leetcode/Solution.java
+++ src/leetcode/Solution.java
@@ -3842,6 +3842,39 @@ public class Solution {
         return dp[text1.length()][text2.length()];
     }
 
+    //编辑距离
+    //给你两个单词 word1 和 word2， 请返回将 word1 转换成 word2 所使用的最少操作数  。
+    //
+    //你可以对一个单词进行如下三种操作：
+    //
+    //插入一个字符
+    //删除一个字符
+    //替换一个字符
+    public int minDistance(String word1, String word2) {
+            int [][]dp = new int[word1.length()+1][word2.length()+1];
+        for (int i = 0; i <= word1.length(); i++) {
+            dp[i][0] = i;
+        }
+        for (int i = 0; i <= word2.length(); i++) {
+            dp[0][i] = i;
+        }
+
+        for (int i = 1; i <= word1.length(); i++) {
+            char c1 = word1.charAt(i-1);
+            for (int j = 1; j <=word2.length(); j++) {
+                char c2 = word2.charAt(j-1);
+
+                if(c1==c2){
+                    dp[i][j] = Math.min(Math.min(dp[i-1][j],dp[i][j-1]),dp[i-1][j-1]);
+                }else {
+                    dp[i][j] = Math.min(Math.min(dp[i-1][j]+1,dp[i][j-1]+1),dp[i-1][j-1]+1);
+                }
+            }
+        }
+
+        return dp[word1.length()][word2.length()];
+    }
+
 
     public static void main(String[] args) throws InterruptedException {
         Solution solution = new Solution();
