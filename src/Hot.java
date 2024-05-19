d6662d2e1b5797420babf5acffa47a4fe176de1d
李磊 <15623219507@163.com>
2024-05-19 15:32:22 +0800

    feat：岛屿数量

--- src/Hot.java
+++ src/Hot.java
@@ -278,6 +278,29 @@ public class Hot {
         return s.substring(left+1,right);
     }
 
+    //岛屿数量
+    public int numIslands(char[][] grid) {
+        int result = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == '1') {
+                    result++;
+                    numIsLands(grid, i, j);
+                }
+            }
+        }
+        return result;
+    }
+
+    public void numIsLands(char[][] grid,int x,int y) {
+        if(x<0||y<0||x>=grid.length||y>=grid[0].length||grid[x][y]=='0') return;
+        grid[x][y]='0';
+        numIsLands(grid,x,y-1);
+        numIsLands(grid,x,y+1);
+        numIsLands(grid,x-1,y);
+        numIsLands(grid,x+1,y);
+    }
+
     public static void main(String[] args) {
 
         Hot hot = new Hot();
