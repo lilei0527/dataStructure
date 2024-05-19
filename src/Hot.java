9d1c277dc0f6a6430de5d52fc39b2efa69d24cac
李磊 <15623219507@163.com>
2024-05-19 21:13:20 +0800

    feat：搜索旋转排序数组

--- src/Hot.java
+++ src/Hot.java
@@ -432,6 +432,32 @@ public class Hot {
         return res;
     }
 
+    //搜索旋转排序数组
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length-1;
+        while(left<=right){
+            int mid = (left+right)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            if(nums[mid]>=nums[0]){//中间节点左侧递增
+                if(nums[0]<=target&&target<nums[mid]){ //要么是左侧，要么右侧
+                    right=mid-1;
+                }else{
+                    left = mid+1;
+                }
+            }else{ //中间节点右侧递增
+                if(nums[mid]<target&&target<=nums[nums.length-1]){
+                    left=mid+1;
+                }else{
+                    right = mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+
 
 
     public static void main(String[] args) {
