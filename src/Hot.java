703bbfbce0084c1e90db59c30c588147946c7b1b
李磊 <15623219507@163.com>
2024-05-19 17:03:09 +0800

    feat：删除链表的倒数第 N 个结点

--- src/Hot.java
+++ src/Hot.java
@@ -341,6 +341,25 @@ public class Hot {
         return res.toArray(new int[res.size()][]);
     }
 
+    //删除链表的倒数第 N 个结点
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode slow = dummy;
+        ListNode fast = head;
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        slow.next = slow.next.next;
+        return dummy.next;
+    }
+
 
 
     public static void main(String[] args) {
