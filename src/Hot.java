ac279c9420a3f57a661506fe93c6f7f283de40b2
lilei <15623219507@163.com>
2024-10-25 11:31:12 +0800

    feat：零钱兑换

--- src/Hot.java
+++ src/Hot.java
@@ -1152,6 +1152,21 @@ public class Hot {
         }
 
 
+        //零钱兑换 (可以重复拿)
+        public int coinChange1(int[] coins, int amount) {
+            int [] dp = new int[amount+1];
+            for (int i = 1; i <= amount; i++) {
+                dp[i] = amount+1;
+                for (int j = 0; j < coins.length; j++) {
+                    if(coins[j]<=i){
+                        dp[i] = Math.min(dp[i],dp[i-coins[j]]+1);
+                    }
+                }
+            }
+            return dp[amount]==amount+1?-1:dp[amount];
+        }
+
+
     public static void main(String[] args) {
         Hot hot = new Hot();
         System.out.println(hot.lengthOfLongestSubstring("tmmzuxt"));
